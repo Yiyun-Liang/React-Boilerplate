144c32f87ed2665b2db1a20c28ab377f260f8e79
isa <isaliangyiyun@hotmail.com>
2017-01-08 14:49:33 -0500

    allow use of es6 features by including stage-0 babel plugin

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,18 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var objOne = {
+  name: 'Isa',
+  location: 'Waterloo'
+};
+
+var objTwo = {
+  age: 19,
+  ...objOne
+}
+
+console.log(objTwo);
+
 ReactDOM.render(
   <h1>Hello React!</h1>,
   document.getElementById('app')
